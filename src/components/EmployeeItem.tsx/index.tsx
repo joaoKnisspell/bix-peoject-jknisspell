807e799c94f53f5999c153103d0d7efd67b4e29b
João Knisspell <99961057+joaoKnisspell@users.noreply.github.com>
2023-10-23 16:57:29 -0300

    Update index.tsx

--- src/components/EmployeeItem.tsx/index.tsx
+++ src/components/EmployeeItem.tsx/index.tsx
@@ -11,7 +11,7 @@ type EmployeeItemProps = {
 export default function EmployeeItem({ id, name, startDate, vacationDate, exitDate }: EmployeeItemProps) {
   return (
     <div className="w-full bg-gray-100 p-2 lg:py-2 lg:px-6 rounded-lg min-w-[39.375rem]">
-      <Link relative="path" to={`/employee/${id}`} className="w-full h-full flex items-center justify-between">
+      <Link relative="path" to={`/employee/${id}`} className="w-full h-full gap-4 flex items-center justify-between">
         <span className="font-medium flex-1">{name}</span>
         <span className="flex-1"><b>Entrada:</b> {startDate}</span>
         <span className="flex-1"><b>Férias:</b> {vacationDate}</span>
