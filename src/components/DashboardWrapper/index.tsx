030efb6bd168f794e78c092d5f0fd63d1c0688bf
João Knisspell <99961057+joaoKnisspell@users.noreply.github.com>
2023-10-23 16:47:37 -0300

    Update index.tsx

--- src/components/DashboardWrapper/index.tsx
+++ src/components/DashboardWrapper/index.tsx
@@ -13,8 +13,8 @@ export default function DashboardWrapper({ title, linkPath, ...props }: Dashboar
   return (
     <section { ...props } className="py-8 px-4 flex flex-col gap-4 overflow-y-hidden">
       <header className="flex w-full items-center justify-between">
-        <h3 className="text-2xl font-medium">{title}</h3>
-        <Link className="flex items-center gap-2 text-gray-600 hover:text-gray-900" to={linkPath}>Ver mais <ArrowRight className="w-4 h-4" /></Link>
+        <h3 className="text-base lg:text-2xl font-medium">{title}</h3>
+        <Link className="lg:flex hidden items-center gap-2 text-gray-600 hover:text-gray-900" to={linkPath}>Ver mais <ArrowRight className="w-4 h-4" /></Link>
       </header>
       <section className="bg-white shadow-lg rounded-lg h-full p-4 overflow-y-scroll flex flex-col gap-5">
         {props.children}
